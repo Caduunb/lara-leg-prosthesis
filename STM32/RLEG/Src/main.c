a83124e7ad53539e163d349452f2dacef78e3efe
Juliana Lemos <lemos.julianab@gmail.com>
2019-06-05 22:34:24 -0300

    encoder funcionando

--- STM32/RLEG/Src/main.c
+++ STM32/RLEG/Src/main.c
@@ -75,7 +75,7 @@ I2C_HandleTypeDef hi2c1;
 
 SPI_HandleTypeDef hspi2;
 
-TIM_HandleTypeDef htim2;
+//TIM_HandleTypeDef htim2;
 
 UART_HandleTypeDef huart1;
 
@@ -98,28 +98,26 @@ uint8_t command, command_rec, data;
 
 void encoder_transmit_command(uint8_t command){ //transmit a command to execute action
 
-	HAL_GPIO_WritePin (GPIOB, GPIO_PIN_12, GPIO_PIN_RESET); // pull the cs pin low
-	HAL_Delay(10);
-	HAL_SPI_Transmit(&hspi2, &command, 1, 100);
-	while(HAL_SPI_GetState(&hspi2) != HAL_SPI_STATE_READY);
-	HAL_Delay(50);
-	HAL_SPI_Receive(&hspi2, &command_rec, 1, 100);
-	HAL_Delay(10);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET); // pull the cs pin low
+	//HAL_Delay(10);
+	HAL_SPI_TransmitReceive(&hspi2, &command, &command_rec, 2, 1);
+	//while(HAL_SPI_GetState(&hspi2) != HAL_SPI_STATE_READY);
+	//HAL_Delay(50);
+	//HAL_SPI_Receive(&hspi2, &command_rec, 1, 100);
+	//HAL_Delay(10);
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET); // pull the cs pin high
 }
 
 void encoder_get_data(uint8_t command){ //transmit a command to read data back
 
 	HAL_GPIO_WritePin (GPIOB, GPIO_PIN_12, GPIO_PIN_RESET); // pull the cs pin low
-	HAL_Delay(10);
-	HAL_SPI_Transmit(&hspi2, &command, 1, 100);
-	while(HAL_SPI_GetState(&hspi2) != HAL_SPI_STATE_READY);
-	HAL_Delay(50);
-	HAL_SPI_Receive(&hspi2, &data, 1, 100);
+	//HAL_Delay(10);
+	HAL_SPI_TransmitReceive(&hspi2, &command, &data, 2, 1);
+	//while(HAL_SPI_GetState(&hspi2) != HAL_SPI_STATE_READY);
+	//HAL_Delay(50);
+	//HAL_SPI_Receive(&hspi2, &data, 1, 100);
 	//HAL_SPI_TransmitReceive (&hspi2, &command, &data, 2, 100); //transmit and receive data
-	HAL_Delay(10);
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET); // pull the cs pin high
-
 }
 
 void encoder_read_pos(){ // reads encoder position
@@ -142,6 +140,8 @@ void encoder_read_pos(){ // reads encoder position
 
 void encoder_init(){
 	int i=0;
+
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET); // pull the cs pin high
 
 	HAL_Delay(500); // wait 500ms for encoder initialization
 
@@ -221,6 +221,8 @@ int main(void)
 	encoder_read_pos();
 	write_data_to_sd(acc_data, gyro_data, position); //writes read data to sd card
 	HAL_Delay(1000); //1000ms delay
+
+
   }
   /* USER CODE END 3 */
 }
@@ -280,7 +282,7 @@ static void MX_I2C1_Init(void)
   hi2c1.Instance = I2C1;
   hi2c1.Init.ClockSpeed = 100000;
   hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
-  hi2c1.Init.OwnAddress1 = 166;
+  hi2c1.Init.OwnAddress1 = 0;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
   hi2c1.Init.OwnAddress2 = 0;
@@ -324,7 +326,6 @@ static void MX_SPI2_Init(void)
   hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
   hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
   hspi2.Init.CRCPolynomial = 10;
-
   if (HAL_SPI_Init(&hspi2) != HAL_OK)
   {
     Error_Handler();
@@ -340,59 +341,59 @@ static void MX_SPI2_Init(void)
   * @param None
   * @retval None
   */
-static void MX_TIM2_Init(void)
-{
-
-  /* USER CODE BEGIN TIM2_Init 0 */
+//static void MX_TIM2_Init(void)
+//{
 //
-  /* USER CODE END TIM2_Init 0 */
-
-  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-  TIM_OC_InitTypeDef sConfigOC = {0};
-
-  /* USER CODE BEGIN TIM2_Init 1 */
+//  /* USER CODE BEGIN TIM2_Init 0 */
+//////
+//  /* USER CODE END TIM2_Init 0 */
 //
-  /* USER CODE END TIM2_Init 1 */
-  htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 9;
-  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 99;
-  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 0;
-  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM2_Init 2 */
+//  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+//  TIM_MasterConfigTypeDef sMasterConfig = {0};
+//  TIM_OC_InitTypeDef sConfigOC = {0};
 //
-  /* USER CODE END TIM2_Init 2 */
-  HAL_TIM_MspPostInit(&htim2);
-
-}
+//  /* USER CODE BEGIN TIM2_Init 1 */
+//////
+//  /* USER CODE END TIM2_Init 1 */
+//  htim2.Instance = TIM2;
+//  htim2.Init.Prescaler = 9;
+//  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+//  htim2.Init.Period = 99;
+//  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+//  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+//  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+//  {
+//    Error_Handler();
+//  }
+//  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+//  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+//  {
+//    Error_Handler();
+//  }
+//  if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
+//  {
+//    Error_Handler();
+//  }
+//  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+//  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+//  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+//  {
+//    Error_Handler();
+//  }
+//  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+//  sConfigOC.Pulse = 0;
+//  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+//  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+//  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+//  {
+//    Error_Handler();
+//  }
+//  /* USER CODE BEGIN TIM2_Init 2 */
+//////
+//  /* USER CODE END TIM2_Init 2 */
+//  HAL_TIM_MspPostInit(&htim2);
+//
+//}
 
 /**
   * @brief USART1 Initialization Function
@@ -434,11 +435,22 @@ static void MX_USART1_UART_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET);
+
+  /*Configure GPIO pin : PB12 */
+  GPIO_InitStruct.Pin = GPIO_PIN_12;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
